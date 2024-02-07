609ab16d58ca09e413f63a82f46ede85aab7304e
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-02-07 15:27:03 -0300

    contador estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contador from "./components/Contador/Contador.jsx"
 function App(){
   return(
     <>
-    <Contador />
+     <Contador /> 
     </>
   )
 }
--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,18 +1,50 @@
-import React, {useState} from "react"//importando o hook useState
+import React, { useState } from "react" //importando o hook useState
+import "./Contador.css"
 
-function Contador(){
+//Hook: Tem funcionalidades e permite manipulações de diversos recursos do react, em um componente funcional.
 
-const [numero, setNumero] = useState(0) //Esse 0 é o valor inicial
+//Hook useState: O hook useState trabalha com estados, e os estados são os que armazenam valores.
+
+function Contador() {
+
+    //Sintaxe do useState
+    const [numero, setNumero] = useState(0)
+
+    //setNumero atualiza/modifica/seta o número
+
+    //Função para adicionar um 1 valor quando eu clicar no botão +
+
+    function Adicionar() {
+
+        if (numero < 20) { //número é menor que 20? se for, faça isso.
+            setNumero(numero + 1) //setNumero(O que você quer fazer?)
+        }
+    }
+
+    //Função para remover um 1 valor quando eu clicar no botão -
+
+    function Remover() {
+        if (numero > 0) {
+            setNumero(numero - 1)
+        }
+    }
 
 
-    return(
-        <>
-        <h1> {numero} </h1>
-        <button> + </button>
-        <button> - </button>
-        <button> C </button>
-        </>
+    //Função para limpar o contador
+    function Limpar() {
+        setNumero(0)
+    }
+
+    return (
+        <section className="contador">
+            <h1 className="numero"> {numero} </h1>
+            <div className="boxButton">
+                <button id="btn1" onClick={Adicionar}> + </button>
+                <button id="btn2" onClick={Remover}> - </button>
+                <button id="btn3" onClick={Limpar}> Limpar</button>
+            </div>
+
+        </section>
     )
 }
-
 export default Contador
